ba14ef60dc14348fce044869e2333231e201beee
Elvis Ndegwa Kangethe <63222738+Elvis888361@users.noreply.github.com>
2025-01-21 19:37:32 +0300

    Update hooks.py

--- boresha_feeds/hooks.py
+++ boresha_feeds/hooks.py
@@ -43,6 +43,9 @@ app_license = "mit"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
+doctype_js = {
+    "Employee" : "public/employee.js",
+}
 # doctype_js = {"doctype" : "public/js/doctype.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
